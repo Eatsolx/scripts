f621d0763d5bbbf8c6e7bacdc8ce077ce5511e48
Eatsolx <xrz@eatsol.net>
2021-12-16 18:47:48 +0800

    路由表与分辨率设置

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,6 @@
 /bin/bash ~/scripts/dwm-status.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
 picom -b
-xrandr --output HDMI-A-2 --mode 1920x1080 --rate 74.97
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 nm-applet &
@@ -13,3 +12,5 @@ xfce4-power-manager &
 ~/scripts/autostart_wait.sh &
 #解除静音
 amixer set Master unmute
+#路由表
+source ~/Desktop/openvpn/add_routes &
--- dualmon.sh
+++ dualmon.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-xrandr --auto --output DP-2 --same-as eDP-1 --size 1920x1080
-xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
+xrandr --output HDMI-A-3 --mode 1920x1080 --rate 74.97
