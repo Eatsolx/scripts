c59fd2e81785b3cc003d2d32045d302f01d37852
Eatsolx <xrz@eatsol.net>
2021-12-18 10:14:56 +0800

    更改位置

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-/bin/bash ~/scripts/dwm-status.sh &
-/bin/bash ~/scripts/wp-autochange.sh &
+/bin/bash ~/Desktop/scripts/dwm-status.sh &
+/bin/bash ~/Desktop/scripts/wp-autochange.sh &
 picom -b
-/bin/bash ~/scripts/tap-to-click.sh &
-/bin/bash ~/scripts/inverse-scroll.sh &
+/bin/bash ~/Desktop/scripts/tap-to-click.sh &
+/bin/bash ~/Desktop/scripts/inverse-scroll.sh &
 nm-applet &
 xfce4-power-manager &
 #xfce4-volumed-pulse &
-/bin/bash ~/scripts/run-mailsync.sh &
-~/scripts/autostart_wait.sh &
+/bin/bash ~/Desktop/scripts/run-mailsync.sh &
+~/Desktop/scripts/autostart_wait.sh &
 #解除静音
 amixer set Master unmute
 #路由表
 source ~/Desktop/openvpn/add_routes &
+#分辨率
+xrandr --output HDMI-A-3 --mode 1920x1080 --rate 74.97
--- dualmon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xrandr --output HDMI-A-3 --mode 1920x1080 --rate 74.97
