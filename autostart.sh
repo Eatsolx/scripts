69d744b85e098b6d872fa1f5fba00d2a7e834393
eatsolx <eatsolx@github.com>
2022-07-12 17:44:36 +0800

    加入消息通知程序自启动

--- autostart.sh
+++ autostart.sh
@@ -10,12 +10,28 @@ xfce4-power-manager &
 #xfce4-volumed-pulse &
 /bin/bash ~/Desktop/scripts/run-mailsync.sh &
 ~/Desktop/scripts/autostart_wait.sh &
+#开启v2raya
+# sudo v2raya &
 #解除静音
-amixer set Master unmute
-#路由表
-source ~/Desktop/openvpn/add_routes &
+# sudo amixer sset Master unmute
+# sudo amixer sset Speaker unmute
+# sudo amixer sset Headphone unmute
+# sudo amixer sset Front unmute
+# sudo amixer sset Surround unmute
+# sudo amixer sset Center unmute
+# sudo amixer sset LFE unmute
+# 
+# 
+# sudo amixer sset Headphone 100
+# sudo amixer sset Front 100
+# sudo amixer sset Surround 100
+# sudo amixer sset Center 100
+# sudo amixer sset LFE 100
 #分辨率
 xrandr --output HDMI-A-0 --mode 1920x1080 --rate 74.97
 xrandr --output HDMI-A-1 --mode 1920x1080 --rate 74.97
 xrandr --output HDMI-A-2 --mode 1920x1080 --rate 74.97
 xrandr --output HDMI-A-3 --mode 1920x1080 --rate 74.97
+
+#启动消息通知程序
+deadd-notification-center &
